34384b3a208fc9a3d785cb505c0684b2974a8c3f
HanSolo1000Falcon <HanSolo1000Falcon.business@gmail.com>
2026-04-02 19:02:45 +0200

    Add function

--- src/executor.cpp
+++ src/executor.cpp
@@ -1,9 +1,12 @@
 #include <vector>
 #include <map>
 #include <string>
-#include <iostream>
 
 #include "../headers/token.hpp"
+
+Token evaluateExpression(const std::vector<Token> &expressionTokens, const std::map<int, std::map<std::string, std::any>> &variablesInScope) {
+    
+}
 
 void executeFunction(const std::vector<Token> &functionInstructions, const std::map<std::string, std::vector<Token>> &functions) {
     int currentScope = 0;
@@ -31,20 +34,6 @@ void executeFunction(const std::vector<Token> &functionInstructions, const std::
                 i += 4;
                 break;
             }
-
-            /*case STANDARD: {
-                const auto& functionName = functionInstructions[i + 2].lexeme;
-
-                if (functionName == "Print") {
-                    const auto& value = functionInstructions[i + 3];
-                    std::cout << std::any_cast<std::string>(functionInstructions[i + 4].value) << '\n';
-                }
-
-                while (functionInstructions[i].type != RIGHT_PAREN) {
-                    ++i;
-                }
-                break;
-            }*/
         }
     }
 }
